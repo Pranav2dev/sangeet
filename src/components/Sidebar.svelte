532e8948c171996828f8c92d013ce3f13e0b1650
Your Name <infowhatsaup@gmail.com>
2026-06-15 22:33:50 +0530

    adding sidebar name / title

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,3 +1,5 @@
 <div class="w-80 px-3 rounded-xl h-full xxl:flex xl:flex lg:flex sm:hidden hidden items-center justify-center">
-    <aside class="bg-[#1d1d1f] h-[95%] w-full rounded-2xl"></aside>
+    <aside class="bg-[#1d1d1f] h-[95%] w-full rounded-2xl">
+        <h3 class="text-white opacity-[0.75] text-2xl font-bold p-3">Sangeet</h3>
+    </aside>
 </div>
